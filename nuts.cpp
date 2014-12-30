8eb2175fe1a9bdb6c3685f3e569a6173228c9eb3
Yumen Cao <gawainus97@gmail.com>
2014-12-30 00:43:38 -0600

    nuts posted

---
+++ nuts.cpp
@@ -0,0 +1,10 @@
+/*
+Problem: given N bolts and N nuts.
+	Bolts cannot be compared to nuts and nuts cannot be compared to nuts.
+	The only way to compare is to fit bolts into nuts.
+	Find the matching nut of each bolt. They are one to one mapped.
+*/
+
+int main() {
+	
+}
